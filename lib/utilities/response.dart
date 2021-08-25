abba40b39ea14e410a549ee0690db73f8248c310
jassiG <jaswant_s@ch.iitr.ac.in>
2021-08-26 01:05:37 +0530

    Added user class

--- lib/utilities/response.dart
+++ lib/utilities/response.dart
@@ -1,3 +1,40 @@
-class response {
-  String name="five";
+class Response {
+  String name = "five";
+  List<User> users = [
+    User("lmao", "this", 5, "this", 5, "this", 5, "this", 5, "this", "this", 5,
+        5, "this")
+  ];
+}
+
+class User {
+  User(
+    this.lastName,
+    this.avatar,
+    this.contribution,
+    this.firstName,
+    this.friendOfCount,
+    this.handle,
+    this.lastOnlineTimeSeconds,
+    this.maxRank,
+    this.maxRating,
+    this.organization,
+    this.rank,
+    this.rating,
+    this.registrationTimeSeconds,
+    this.titlePhoto,
+  );
+  String lastName;
+  int lastOnlineTimeSeconds;
+  int rating;
+  int friendOfCount;
+  String titlePhoto;
+  String handle;
+  String avatar;
+  String firstName;
+  int contribution;
+  String organization;
+  String rank;
+  int maxRating;
+  int registrationTimeSeconds;
+  String maxRank;
 }
